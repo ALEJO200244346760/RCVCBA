e998ff96fb1786ce46a01a9d500317335a15dec8
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-10-03 18:18:42 -0300

    Primer commit

--- rcv_Backend/src/main/java/com/backend/rcv/config/WebConfig.java
+++ rcv_Backend/src/main/java/com/backend/rcv/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://rcvcba.vercel.app")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
