9bcbdb15e22a7035dbffa3ba3d17dfd866891210
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-10-22 01:58:33 -0300

    editar PACIENTE

--- riesgo-cardiovascular/src/components/EditarPaciente.jsx
+++ riesgo-cardiovascular/src/components/EditarPaciente.jsx
@@ -152,7 +152,7 @@ function EditarPaciente() {
 
   const manejarSubmit = async (e) => {
     e.preventDefault();
-    console.log('Datos a enviar:', datosPaciente); // Añade esto para depurar
+    console.log(datosPaciente); // Añadir este log
     try {
       await axios.put(`https://rcvcba-production.up.railway.app/api/pacientes/${id}`, datosPaciente, {
         headers: {
@@ -165,7 +165,6 @@ function EditarPaciente() {
       console.error('Error al actualizar el paciente:', error);
     }
   };
-  
 
   if (loading) return <p>Cargando...</p>;
 
