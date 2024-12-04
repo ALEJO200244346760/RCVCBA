686206070d199879250e1bfd9524e98f721d6d6c
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2024-12-04 13:47:27 -0300

    copiar

--- riesgo-cardiovascular/src/components/Formulario.jsx
+++ riesgo-cardiovascular/src/components/Formulario.jsx
@@ -1254,7 +1254,7 @@ const Formulario = () => {
 
             {/* Modal para agregar medicamentos */}
             {mostrarModalMedicamentos && (
-                <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+                <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center max-h-[90vh]">
                     <div className="bg-white p-6 rounded-md shadow-lg w-10/12 max-w-2xl max-h-[90vh]">
                         <h2 className="text-lg font-semibold mb-4">SIGIPSA</h2>
                         <div className="mb-4 max-h-96 overflow-y-auto">
