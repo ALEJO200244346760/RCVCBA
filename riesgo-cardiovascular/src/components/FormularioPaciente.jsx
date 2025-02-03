444d5f1163f84926ba017dbd274b1a166e32f216
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-02-03 19:19:49 -0300

    CUS

--- riesgo-cardiovascular/src/components/FormularioPaciente.jsx
+++ riesgo-cardiovascular/src/components/FormularioPaciente.jsx
@@ -222,7 +222,7 @@ const FormularioPaciente = ({ roles }) => {
             )}
     
             {/* Si el paciente tiene datos de enfermería, mostramos el formulario de cardiología */}
-            {(isCardiologo || isCardiologia) && esPacienteNuevo === false && datosEnfermeria && (
+            {esPacienteNuevo === false && (isCardiologo || isCardiologia) && datosEnfermeria && (
                         <form onSubmit={manejarSubmitCardiologia} className="w-full space-y-6">
                             <h2 className="text-xl font-bold mb-4">Datos de Cardiología</h2>
     
