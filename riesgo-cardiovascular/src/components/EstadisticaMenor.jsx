7491722def58ec6e61e6cd2fa9acacda51b26af1
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-02-06 00:39:53 -0300

    CUS

--- riesgo-cardiovascular/src/components/EstadisticaMenor.jsx
+++ riesgo-cardiovascular/src/components/EstadisticaMenor.jsx
@@ -1,4 +1,3 @@
-// src/components/EstadisticaMenor.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import useAuthToken from '../hooks/useAuthToken'; // Asegúrate de importar el hook para acceder al token
@@ -17,16 +16,19 @@ const EstadisticaMenor = () => {
       try {
         const response = await axios.get('/api/pacientemenor', {
           headers: {
-            Authorization: `Bearer ${token}`,  // Aquí estamos añadiendo el token correctamente
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
+
+        console.log('Respuesta de la API:', response.data); // Para inspeccionar la respuesta
+
         const data = response.data;
 
-        // Verificamos que la respuesta sea un array
-        if (Array.isArray(data)) {
+        // Verificamos si la respuesta es un array válido y contiene elementos
+        if (Array.isArray(data) && data.length > 0) {
           setPacientes(data);
         } else {
-          throw new Error('La respuesta no contiene un array válido de pacientes');
+          throw new Error('La respuesta no contiene un array válido de pacientes o está vacío');
         }
       } catch (err) {
         setError(err.response ? err.response.data.message : err.message);
@@ -35,15 +37,13 @@ const EstadisticaMenor = () => {
       }
     };
 
-    if (token) {
-      fetchPacientes();
-    }
-  }, [token]); // El efecto se ejecuta cada vez que el token cambia
+    fetchPacientes();
+  }, [token]);
 
   const toggleDetalles = (id) => {
     setMostrarDetalles((prevState) => ({
       ...prevState,
-      [id]: !prevState[id]
+      [id]: !prevState[id],
     }));
   };
 
@@ -57,36 +57,40 @@ const EstadisticaMenor = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {pacientes.slice().reverse().map(paciente => (
+      {pacientes.slice().reverse().map((paciente) => (
         <div key={paciente.dni} className="bg-white shadow-md rounded-lg p-4">
           {/* Datos siempre visibles */}
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">DNI:</div>
-            <div className="text-sm text-gray-500">{paciente.dni || 'null'}</div>
+            <div className="text-sm text-gray-500">{paciente.dni || 'No disponible'}</div>
           </div>
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">Género:</div>
-            <div className="text-sm text-gray-500">{paciente.genero || 'null'}</div>
+            <div className="text-sm text-gray-500">{paciente.genero || 'No disponible'}</div>
           </div>
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">Peso:</div>
-            <div className="text-sm text-gray-500">{paciente.peso || 'null'}</div>
+            <div className="text-sm text-gray-500">{paciente.peso || 'No disponible'}</div>
           </div>
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">Talla:</div>
-            <div className="text-sm text-gray-500">{paciente.talla || 'null'}</div>
+            <div className="text-sm text-gray-500">{paciente.talla || 'No disponible'}</div>
           </div>
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">Tensión Arterial:</div>
-            <div className="text-sm text-gray-500">{paciente.tensionArterial || 'null'}</div>
+            <div className="text-sm text-gray-500">{paciente.tensionArterial || 'No disponible'}</div>
           </div>
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">Hipertenso:</div>
-            <div className="text-sm text-gray-500">{paciente.hipertenso !== undefined ? (paciente.hipertenso ? 'Sí' : 'No') : 'null'}</div>
+            <div className="text-sm text-gray-500">
+              {paciente.hipertenso !== undefined ? (paciente.hipertenso ? 'Sí' : 'No') : 'No disponible'}
+            </div>
           </div>
           <div className="flex justify-between items-start mb-2">
             <div className="text-sm font-medium text-gray-900">Diabetes:</div>
-            <div className="text-sm text-gray-500">{paciente.diabetes !== undefined ? (paciente.diabetes ? 'Sí' : 'No') : 'null'}</div>
+            <div className="text-sm text-gray-500">
+              {paciente.diabetes !== undefined ? (paciente.diabetes ? 'Sí' : 'No') : 'No disponible'}
+            </div>
           </div>
 
           {/* Mostrar detalles adicionales solo si están activados */}
@@ -94,23 +98,23 @@ const EstadisticaMenor = () => {
             <div className="mt-2">
               <div className="flex justify-between items-start mb-2">
                 <div className="text-sm font-medium text-gray-900">Asma:</div>
-                <div className="text-sm text-gray-500">{paciente.asma || 'null'}</div>
+                <div className="text-sm text-gray-500">{paciente.asma || 'No disponible'}</div>
               </div>
               <div className="flex justify-between items-start mb-2">
                 <div className="text-sm font-medium text-gray-900">Fuma:</div>
-                <div className="text-sm text-gray-500">{paciente.fuma || 'null'}</div>
+                <div className="text-sm text-gray-500">{paciente.fuma || 'No disponible'}</div>
               </div>
               <div className="flex justify-between items-start mb-2">
                 <div className="text-sm font-medium text-gray-900">Arritmias:</div>
-                <div className="text-sm text-gray-500">{paciente.arritmias || 'null'}</div>
+                <div className="text-sm text-gray-500">{paciente.arritmias || 'No disponible'}</div>
               </div>
               <div className="flex justify-between items-start mb-2">
                 <div className="text-sm font-medium text-gray-900">Cirugía Previa:</div>
-                <div className="text-sm text-gray-500">{paciente.cirugiaPrevia || 'null'}</div>
+                <div className="text-sm text-gray-500">{paciente.cirugiaPrevia || 'No disponible'}</div>
               </div>
               <div className="flex justify-between items-start mb-2">
                 <div className="text-sm font-medium text-gray-900">Alergias:</div>
-                <div className="text-sm text-gray-500">{paciente.alergias || 'null'}</div>
+                <div className="text-sm text-gray-500">{paciente.alergias || 'No disponible'}</div>
               </div>
             </div>
           )}
@@ -119,7 +123,6 @@ const EstadisticaMenor = () => {
           <button onClick={() => toggleDetalles(paciente.dni)} className="text-indigo-600 hover:text-indigo-900 mt-2">
             {mostrarDetalles[paciente.dni] ? 'Mostrar menos' : 'Mostrar más'}
           </button>
-
         </div>
       ))}
     </div>
