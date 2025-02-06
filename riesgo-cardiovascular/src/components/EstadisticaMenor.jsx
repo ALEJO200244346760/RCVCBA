6274032341f47c286eeab363d509ac69b5dfd80a
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-02-05 23:57:20 -0300

    CUS

--- riesgo-cardiovascular/src/components/EstadisticaMenor.jsx
+++ riesgo-cardiovascular/src/components/EstadisticaMenor.jsx
@@ -5,19 +5,17 @@ const EstadisticaMenor = () => {
   const [pacientes, setPacientes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [mostrarDetalles, setMostrarDetalles] = useState({});
 
-  // Llamar al API para obtener los pacientes menores
   useEffect(() => {
     const fetchPacientes = async () => {
       try {
-        const response = await axios.get('/api/pacientemenor'); // Suponiendo que el endpoint para obtener los pacientes está en esta ruta
+        const response = await axios.get('/api/pacientemenor'); // Asegúrate de que esta ruta esté correcta
         const data = response.data;
-
-        // Si la respuesta tiene una propiedad 'pacientes' que contiene el array
-        if (Array.isArray(data.pacientes)) {
-          setPacientes(data.pacientes); // Usamos 'data.pacientes' si la respuesta es un objeto con 'pacientes'
-        } else if (Array.isArray(data)) {
-          setPacientes(data); // Si la respuesta es directamente un array
+        
+        // Verificamos que la respuesta sea un array
+        if (Array.isArray(data)) {
+          setPacientes(data);
         } else {
           throw new Error('La respuesta no contiene un array válido de pacientes');
         }
@@ -27,8 +25,16 @@ const EstadisticaMenor = () => {
         setLoading(false);
       }
     };
+
     fetchPacientes();
   }, []);
+
+  const toggleDetalles = (id) => {
+    setMostrarDetalles((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id]
+    }));
+  };
 
   if (loading) {
     return <div>Cargando...</div>;
@@ -39,43 +45,72 @@ const EstadisticaMenor = () => {
   }
 
   return (
-    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {pacientes.length > 0 ? (
-        pacientes.map((paciente) => (
-          <div
-            key={paciente.dni}
-            className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center"
-          >
-            {/* Foto de avatar usando el nombre */}
-            <img
-              src={`https://ui-avatars.com/api/?name=${paciente.dni}&background=random`}
-              alt={paciente.dni}
-              className="w-24 h-24 rounded-full mb-4"
-            />
-            <h3 className="text-xl font-semibold text-center">{paciente.dni}</h3>
-            <p className="text-sm text-gray-600">Género: {paciente.genero}</p>
-            <p className="text-sm text-gray-600">Peso: {paciente.peso} kg</p>
-            <p className="text-sm text-gray-600">Talla: {paciente.talla} cm</p>
-            <p className="text-sm text-gray-600">Tensión Arterial: {paciente.tensionArterial} mmHg</p>
-            <p className="text-sm text-gray-600">Hipertenso: {paciente.hipertenso ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Diabetes: {paciente.diabetes ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Asma: {paciente.asma ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Fuma: {paciente.fuma ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Antecedentes Soplo: {paciente.antecedentesSoplo ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Arritmias: {paciente.arritmias ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Enfermedad Crónica: {paciente.enfermedadCronica ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Cirugía Previa: {paciente.cirugiaPrevia ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Alergias: {paciente.alergias ? paciente.alergias : 'No'}</p>
-            <p className="text-sm text-gray-600">Antecedentes Familiares Marcapaso: {paciente.antecedentesFamiliaresMarcapaso ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Desfibriladores: {paciente.desfibriladores ? 'Sí' : 'No'}</p>
-            <p className="text-sm text-gray-600">Tensión Arterial Máxima: {paciente.tensionArterialMaxima} mmHg</p>
-            <p className="text-sm text-gray-600">Tensión Arterial Mínima: {paciente.tensionArterialMinima} mmHg</p>
-            <p className="text-sm text-gray-600">Electrocardiograma: {paciente.electrocardiograma ? 'Normal' : 'Anómalo'}</p>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {pacientes.slice().reverse().map(paciente => (
+        <div key={paciente.dni} className="bg-white shadow-md rounded-lg p-4">
+          {/* Datos siempre visibles */}
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">DNI:</div>
+            <div className="text-sm text-gray-500">{paciente.dni || 'null'}</div>
           </div>
-        ))
-      ) : (
-        <p>No hay pacientes registrados.</p>
-      )}
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">Género:</div>
+            <div className="text-sm text-gray-500">{paciente.genero || 'null'}</div>
+          </div>
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">Peso:</div>
+            <div className="text-sm text-gray-500">{paciente.peso || 'null'}</div>
+          </div>
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">Talla:</div>
+            <div className="text-sm text-gray-500">{paciente.talla || 'null'}</div>
+          </div>
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">Tensión Arterial:</div>
+            <div className="text-sm text-gray-500">{paciente.tensionArterial || 'null'}</div>
+          </div>
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">Hipertenso:</div>
+            <div className="text-sm text-gray-500">{paciente.hipertenso !== undefined ? (paciente.hipertenso ? 'Sí' : 'No') : 'null'}</div>
+          </div>
+          <div className="flex justify-between items-start mb-2">
+            <div className="text-sm font-medium text-gray-900">Diabetes:</div>
+            <div className="text-sm text-gray-500">{paciente.diabetes !== undefined ? (paciente.diabetes ? 'Sí' : 'No') : 'null'}</div>
+          </div>
+
+          {/* Mostrar detalles adicionales solo si están activados */}
+          {mostrarDetalles[paciente.dni] && (
+            <div className="mt-2">
+              <div className="flex justify-between items-start mb-2">
+                <div className="text-sm font-medium text-gray-900">Asma:</div>
+                <div className="text-sm text-gray-500">{paciente.asma || 'null'}</div>
+              </div>
+              <div className="flex justify-between items-start mb-2">
+                <div className="text-sm font-medium text-gray-900">Fuma:</div>
+                <div className="text-sm text-gray-500">{paciente.fuma || 'null'}</div>
+              </div>
+              <div className="flex justify-between items-start mb-2">
+                <div className="text-sm font-medium text-gray-900">Arritmias:</div>
+                <div className="text-sm text-gray-500">{paciente.arritmias || 'null'}</div>
+              </div>
+              <div className="flex justify-between items-start mb-2">
+                <div className="text-sm font-medium text-gray-900">Cirugía Previa:</div>
+                <div className="text-sm text-gray-500">{paciente.cirugiaPrevia || 'null'}</div>
+              </div>
+              <div className="flex justify-between items-start mb-2">
+                <div className="text-sm font-medium text-gray-900">Alergias:</div>
+                <div className="text-sm text-gray-500">{paciente.alergias || 'null'}</div>
+              </div>
+            </div>
+          )}
+
+          {/* Botón "Mostrar más" o "Mostrar menos" */}
+          <button onClick={() => toggleDetalles(paciente.dni)} className="text-indigo-600 hover:text-indigo-900 mt-2">
+            {mostrarDetalles[paciente.dni] ? 'Mostrar menos' : 'Mostrar más'}
+          </button>
+
+        </div>
+      ))}
     </div>
   );
 };
